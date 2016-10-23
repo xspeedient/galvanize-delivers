dd40990b7cd513ddb68629b7c2929d6b87fce9a2
Sam Scherf <xspeedient@gmail.com>
2016-10-22 21:47:03 -0500

    adding jquery name space

--- starter-template/js/init.js
+++ starter-template/js/init.js
@@ -2,30 +2,31 @@ function numberfy(stringPrice) {
   return parseInt(stringPrice.slice(0, -1));
 }
 
-$(function(){
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav();
+    $('.card-action').click(function () {
+      var itemName = $(this).prev().children('span').text();
+      var itemPrice = $(this).prev().children('p').text();
+      $('#orderList').append('<li class="collection-item"><span class="itemName">'+itemName+'</span><span class="floatRight">'+itemPrice+'</span></li>');
+      var numPrice = numberfy(itemPrice);
+      var currentSubtotal = numberfy($('#subtotal').text());
+      var newSubtotal = numPrice + currentSubtotal;
+      $('#subtotal').text(newSubtotal + 'Ƀ');
+      var tax = (newSubtotal * .075);
+      $('#tax').text((tax).toFixed(2) + 'Ƀ');
+      $('#total').text((newSubtotal+tax).toFixed(2) + 'Ƀ');
+    });
 
-  $('.button-collapse').sideNav();
-  $('.card-action').click(function () {
-    var itemName = $(this).prev().children('span').text();
-    var itemPrice = $(this).prev().children('p').text();
-    $('#orderList').append('<li class="collection-item"><span class="itemName">'+itemName+'</span><span class="floatRight">'+itemPrice+'</span></li>');
-    var numPrice = numberfy(itemPrice);
-    var currentSubtotal = numberfy($('#subtotal').text());
-    var newSubtotal = numPrice + currentSubtotal;
-    $('#subtotal').text(newSubtotal + 'Ƀ');
-    var tax = (newSubtotal * .075);
-    $('#tax').text((tax).toFixed(2) + 'Ƀ');
-    $('#total').text((newSubtotal+tax).toFixed(2) + 'Ƀ');
-  });
-
-  $('#submitOrder').click(function (e) {
-    e.preventDefault();
-    if ($('#orderList li').length <= 1) {
-      Materialize.toast('Please select an item before you submit your order!', 4000);
-    } else if ($('#icon_prefix').val() == 0 || $('#icon_telephone').val() == 0 || $('#icon_business').val() == 0 ) {
-      Materialize.toast('Please fill out all required forms', 4000);
-    } else {
-      Materialize.toast('Order submitted!', 4000);
-    }
-  });
-}); // end of document ready
+    $('#submitOrder').click(function (e) {
+      e.preventDefault();
+      if ($('#orderList li').length <= 1) {
+        Materialize.toast('Please select an item before you submit your order!', 4000);
+      } else if ($('#icon_prefix').val() == 0 || $('#icon_telephone').val() == 0 || $('#icon_business').val() == 0 ) {
+        Materialize.toast('Please fill out all required forms', 4000);
+      } else {
+        Materialize.toast('Order submitted!', 4000);
+      }
+    });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
